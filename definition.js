17e59fd6ad1dabe26656fc18b8c5615dfab18dec
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-03-04 23:07:16 +0700

    d

--- definition.js
+++ definition.js
@@ -72,3 +72,32 @@ Blockly.Python['read_relay_status_at_address'] = function (block) {
   
   return [code, Blockly.Python.ORDER_ATOMIC];
 };
+
+Blockly.Blocks['scan_and_add_card'] = {
+  init: function() {
+    this.jsonInit({
+      "type": "scan_and_add_card",
+      "message0": "quét và thêm thẻ RFID vào danh sách %1%2",
+      "args0": [
+        {
+          "type": "input_value",
+          "name": "LISTNAME",
+          "check": "String"
+        },
+        { "type": "input_dummy" }
+      ],
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": "#00aeae",
+      "tooltip": "Quét thẻ RFID và thêm vào danh sách",
+      "helpUrl": ""
+    });
+  }
+};
+
+Blockly.Python['scan_and_add_card'] = function(block) {
+  var listname = Blockly.Python.valueToCode(block, 'LISTNAME', Blockly.Python.ORDER_ATOMIC) || "''";
+  Blockly.Python.definitions_['import_rfid'] = 'from rfid import *'; 
+  var code = "rfid.scan_and_add_card(" + listname + ")\n";
+  return code;
+};
