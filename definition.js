387edffc9ffae87cd9d981b75421fbe139e7500a
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-03-04 23:11:27 +0700

    d

--- definition.js
+++ definition.js
@@ -39,40 +39,6 @@ Blockly.Blocks['scan_and_add_card'] = {
   }
 };
 
-
-Blockly.Blocks['read_relay_status_at_address'] = {
-  init: function () {
-    this.jsonInit({
-      "type": "read_relay_status_at_address",
-      "message0": "kênh đọc trạng thái địa chỉ %1%2",
-      "args0": [
-        {
-          "type": "input_value",
-          "name": "address",
-          "check": "Number"
-        },
-        { "type": "input_dummy" }
-      ],
-      "output": "Number",
-      "colour": "#18820c",
-      "tooltip": "Đọc trạng thái của relay tại địa chỉ được nhập",
-      "helpUrl": ""
-    });
-  }
-};
-
-Blockly.Python['read_relay_status_at_address'] = function (block) {
-  Blockly.Python.definitions_['import_relay_driver'] = 'from relay_4chs import *';
-  
-  var relay = block.getFieldValue('relay');
-  var address = Blockly.Python.valueToCode(block, 'address', Blockly.Python.ORDER_ATOMIC);
-
-  Blockly.Python.definitions_['relay_init_' + address] = `relay_${address} = RelayController(${address})`;  
-  var code = `relay_${address}.get_relay(${relay})\n`;  // Đọc trạng thái relay
-  
-  return [code, Blockly.Python.ORDER_ATOMIC];
-};
-
 Blockly.Blocks['scan_and_add_card'] = {
   init: function() {
     this.jsonInit({
