37436a033a591291906577200ab1ddaf3d1e74fa
Nguyễn Văn Kiên <138760549+KIEN20030806@users.noreply.github.com>
2025-03-04 22:02:22 +0700

    x

--- definition.js
+++ definition.js
@@ -17,75 +17,30 @@ Blockly.Python['scan_card'] = function(block) {
   return code;
 };
 
-Blockly.Blocks['veml6040_sensor_detect_color'] = {
+Blockly.Blocks['scan_and_add_card'] = {
   init: function() {
     this.jsonInit({
-      "type": "veml6040_sensor_detect_color",
-      "message0": "cảm biến VEML6040 phát hiện màu %1",
+      "type": "scan_and_add_card",
+      "message0": "quét và thêm thẻ RFID vào danh sách %1",
       "args0": [
         {
-          "type": "field_dropdown",
-          "name": "DETECT_COLOR",
-          "options": [
-            ["vàng", "yellow"],
-            ["đỏ", "red"],
-            ["xanh lá", "green"],
-            ["xanh lơ", "cyan"],
-            ["xanh dương", "blue"],
-            ["hồng thẫm", "magenta"]
-          ]
+          "type": "input_value",
+          "name": "LISTNAME",
+          "check": "String"
         }
       ],
-      "output": "Boolean",
-      "colour": "#ae00ae",
-      "tooltip": "Phát hiện màu sắc cụ thể",
+      "previousStatement": null,
+      "nextStatement": null,
+      "colour": "#00aeae",
+      "tooltip": "Quét thẻ RFID và thêm vào danh sách",
       "helpUrl": ""
     });
   }
 };
 
-Blockly.Python['veml6040_sensor_detect_color'] = function(block) {
-  var detectColor = block.getFieldValue('DETECT_COLOR');
-  var code = '(veml6040_sensor.Classify_Hue() == "' + detectColor + '")';
-  return [code, Blockly.Python.ORDER_ATOMIC];
+Blockly.Python['scan_and_add_card'] = function(block) {
+  var listname = Blockly.Python.valueToCode(block, 'LISTNAME', Blockly.Python.ORDER_ATOMIC) || "''";
+  Blockly.Python.definitions_['import_rfid'] = 'from rfid import *'; 
+  var code = "rfid.scan_and_add_card(" + listname + ")\n";
+  return code;
 };
-
-
-
-Blockly.Blocks['veml6040_sensor_read_lux'] = {
-  init: function() {
-    this.jsonInit({
-      "type": "veml6040_sensor_read_lux",
-      "message0": "cảm biến VEML6040 đọc độ sáng (lux)", 
-      "output": "Number",
-      "colour": "#ae00ae",
-      "tooltip": "Đọc giá trị độ sáng lux",
-      "helpUrl": ""
-    });
-  }
-};
-
-Blockly.Python['veml6040_sensor_read_lux'] = function(block) {
-  var code = 'veml6040_sensor.get_lux()'; 
-  return [code, Blockly.Python.ORDER_ATOMIC];
-};
-
-Blockly.Blocks['veml6040_sensor_read_cct'] = {
-  init: function() {
-    this.jsonInit({
-      "type": "veml6040_sensor_read_cct",
-      "message0": "cảm biến VEML6040 đọc nhiệt độ màu",
-      "output": "Number",
-      "colour": "#ae00ae",
-      "tooltip": "Đọc giá trị nhiệt độ màu (CCT)",
-      "helpUrl": ""
-    });
-  }
-};
-
-Blockly.Python['veml6040_sensor_read_cct'] = function(block) {
-  var code = 'veml6040_sensor.get_cct()'; 
-  return [code, Blockly.Python.ORDER_ATOMIC];
-};
-
-Blockly.Python.definitions_['import_veml6040_sensor'] = 'from veml6040_sensor import VEML6040Sensor';
